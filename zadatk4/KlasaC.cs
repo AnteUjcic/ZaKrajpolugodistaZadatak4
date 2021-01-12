cdb9182b2cc5cf0bb425e76e55499719ed54716c
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-01-12 16:33:09 +0100

    Add files via upload

---
+++ zadatk4/KlasaC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatk4
+{
+   
+    class KlasaC
+    {
+    
+    
+    
+        public int Zbroj(int[] polje)
+        {
+
+            int priv=0;
+            for(int i = 0; i <= 4; i++)
+            {
+                priv = polje[i] + priv;
+
+            }
+
+            return priv;
+                
+
+        }
+        public double prosjek(int []poljeZaProsjek)
+        {
+            int privremenaVarijablaZaRacunanjeProsjekaBrojeva = 0;
+            for (int i = 0; i <= 4; i++)
+            {
+                privremenaVarijablaZaRacunanjeProsjekaBrojeva = poljeZaProsjek[i] + privremenaVarijablaZaRacunanjeProsjekaBrojeva;
+
+            }
+            privremenaVarijablaZaRacunanjeProsjekaBrojeva = privremenaVarijablaZaRacunanjeProsjekaBrojeva / poljeZaProsjek.Length;
+
+            return privremenaVarijablaZaRacunanjeProsjekaBrojeva;
+        }
+
+
+
+    }
+}
